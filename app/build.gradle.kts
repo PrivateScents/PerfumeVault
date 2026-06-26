de31645017483a1ea8e56e56c6d5fa01fc893059
Billensteiner <lukas.billensteiner@htlstp.at>
2026-06-26 16:12:57 +0200

    v16 fix version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.perfumevault"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
+        versionCode = 16
         versionName = "9.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
